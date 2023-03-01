39c6e83fb8b67369583837a23e1d262f8610b39d
santgonnzalez <santiago.gonnzalez1@gmail.com>
2023-03-01 16:17:54 -0300

    first commit using react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vendo Bitcoin 100 el g
         </p>
         <a
           className="App-link"
